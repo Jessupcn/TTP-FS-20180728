6f6afda644afd05286e6a76c69a9f501ac5755e4
Connor Jessup <cjessup@umich.edu>
2018-11-25 19:47:17 -0500

    ADDED: findColor() to change text color for portfolio

--- Client/Components/PortfolioComponents/PortfolioItem.js
+++ Client/Components/PortfolioComponents/PortfolioItem.js
@@ -6,19 +6,24 @@ import React from 'react';
 const PortfolioItem = props => {
   const { tickerSymbol, quantity, currentPrice, openPrice } = props.asset;
   return (
-    <div
-      className={
-        openPrice > currentPrice
-          ? 'red flexRow singleAsset'
-          : 'green flexRow singleAsset'
-      }
-    >
+    <div className={this.findColor(currentPrice, openPrice)}>
       <p>{tickerSymbol}</p>
       <p>{quantity}</p>
       <p>{`$${currentPrice.toFixed(2)}`}</p>
       <p>{`$${(currentPrice * quantity).toFixed(2)}`}</p>
     </div>
   );
+};
+
+// Returns class names with proper color for text
+PortfolioItem.prototype.findColor = (currentPrice, openPrice) => {
+  if (openPrice > currentPrice) {
+    return 'red flexRow singleAsset';
+  } else if (currentPrice > openPrice) {
+    return 'green flexRow singleAsset';
+  } else {
+    return 'grey flexRow singleAsset';
+  }
 };
 
 export default PortfolioItem;
